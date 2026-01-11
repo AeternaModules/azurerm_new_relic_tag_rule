3397db81f348b4f666f7ec084320dcfaac0196a4
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:55:38 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "new_relic_tag_rules" {
+  description = <<EOT
+Map of new_relic_tag_rules, attributes below
+Required:
+    - monitor_id
+Optional:
+    - activity_log_enabled
+    - azure_active_directory_log_enabled
+    - metric_enabled
+    - subscription_log_enabled
+    - log_tag_filter (block):
+        - action (required)
+        - name (required)
+        - value (required)
+    - metric_tag_filter (block):
+        - action (required)
+        - name (required)
+        - value (required)
+EOT
+
+  type = map(object({
+    monitor_id                         = string
+    activity_log_enabled               = optional(bool, false)
+    azure_active_directory_log_enabled = optional(bool, false)
+    metric_enabled                     = optional(bool, false)
+    subscription_log_enabled           = optional(bool, false)
+    log_tag_filter = optional(object({
+      action = string
+      name   = string
+      value  = string
+    }))
+    metric_tag_filter = optional(object({
+      action = string
+      name   = string
+      value  = string
+    }))
+  }))
+}
+
